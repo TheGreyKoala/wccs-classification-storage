ef2aa50fbdc2708f8c56c7cf64ea7b61a9c841a1
Tim Gremplewski <tim.gremplewski@gmail.com>
2017-07-08 16:44:10 +0200

    Create unique node property

--- custom-entrypoint.sh
+++ custom-entrypoint.sh
@@ -1,18 +1,23 @@
 #!/bin/bash -eu
 
 function setupNeo4j() {
-	echo "Waiting 10 seconds for neo4j to come up."
+    timeout=30
 
-	end="$((SECONDS+10))"
+	echo "Waiting $timeout seconds for Neo4j to come up."
+	end="$((SECONDS+timeout))"
 
-	while true; do
+	while true
+	do
 	    [[ "200" = "$(curl --silent --write-out %{http_code} --output /dev/null http://localhost:7474)" ]] && break
-	    [[ "${SECONDS}" -ge "${end}" ]] && echo "neo4j did not come up after 10 seconds." && exit 1
+	    [[ "${SECONDS}" -ge "${end}" ]] && echo "Neo4j did not come up after $timeout seconds." && exit 1
 	    sleep 1
 	done
 
-	echo "Assuring Constraints"
-	exit 0
+	echo "Creating unique node property constraint for label 'Page' and property 'url'."
+	/var/lib/neo4j/bin/cypher-shell -u neo4j -p $(echo $NEO4J_AUTH | cut -f2 -d/) "CREATE CONSTRAINT ON (page:Page) ASSERT page.url IS UNIQUE"
+
+	# Property existence required to assure that each node labeled 'page' has a property 'url.
+	# But this feature is not available in the community edition.
 }
 
 setupNeo4j &
