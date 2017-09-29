31739ffd28670c08b7dd0298f32ff8281d414af6
Tim Gremplewski <tim.gremplewski@gmail.com>
2017-09-29 09:06:41 +0200

    WCCS-20 Updated constraints

--- custom-entrypoint.sh
+++ custom-entrypoint.sh
@@ -13,11 +13,17 @@ function setupNeo4j() {
 	    sleep 1
 	done
 
+    echo "Creating unique node property constraint :Content(value)"
+    /var/lib/neo4j/bin/cypher-shell -u neo4j -p $(echo $NEO4J_AUTH | cut -f2 -d/) "CREATE CONSTRAINT ON (content:Content) ASSERT content.value IS UNIQUE"
+
+    echo "Creating unique node property constraint :Property(checksum)"
+    /var/lib/neo4j/bin/cypher-shell -u neo4j -p $(echo $NEO4J_AUTH | cut -f2 -d/) "CREATE CONSTRAINT ON (property:Property) ASSERT property.checksum IS UNIQUE"
+
 	echo "Creating unique node property constraint :Resource(url)"
 	/var/lib/neo4j/bin/cypher-shell -u neo4j -p $(echo $NEO4J_AUTH | cut -f2 -d/) "CREATE CONSTRAINT ON (resource:Resource) ASSERT resource.url IS UNIQUE"
 
-	echo "Creating unique node property constraint :Site(name)"
-	/var/lib/neo4j/bin/cypher-shell -u neo4j -p $(echo $NEO4J_AUTH | cut -f2 -d/) "CREATE CONSTRAINT ON (site:Site) ASSERT site.name IS UNIQUE"
+	echo "Creating unique node property constraint :Site(id)"
+	/var/lib/neo4j/bin/cypher-shell -u neo4j -p $(echo $NEO4J_AUTH | cut -f2 -d/) "CREATE CONSTRAINT ON (site:Site) ASSERT site.id IS UNIQUE"
 
 	# Property existence required to assure that each node labeled 'page' has a property 'url.
 	# But this feature is not available in the community edition.
